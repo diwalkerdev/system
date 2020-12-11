9ea82590b3718910af3c7fbb7822c082214450bd
David Walker <dwalker0044@gmail.com>
2020-12-11 08:44:22 +0000

    Updated fish prompt.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,6 +1,3 @@
-# name: Classic + Vcs
-# author: Lily Ballard
-
 function fish_prompt --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
     set -l normal (set_color normal)
@@ -8,12 +5,12 @@ function fish_prompt --description 'Write out the prompt'
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
     set -l prefix
-    set -l suffix ' >>'
+    set -l suffix '>>'
     if contains -- $USER root toor
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
         end
-        set suffix ' #>'
+        set suffix '##'
     end
 
     # If we're running via SSH, change the host color.
@@ -25,9 +22,5 @@ function fish_prompt --description 'Write out the prompt'
     # Write pipestatus
     set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
 
-    if set -q SSH_TTY
-	    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status $suffix " "
-    else
-	    echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status $suffix " "
-    end
+    echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal $prompt_status " " $suffix " "
 end
