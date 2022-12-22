3aabe01477e97b8e4d68248e829f632172340d4a
David Walker <dwalker0044@gmail.com>
2022-12-22 16:02:19 +0000

    Deleted install-submodules.

--- install-submodules.bash
+++
@@ -1,7 +0,0 @@
-# Pulls any submodules into the project and runs any install scripts required to
-# install the submodules.
-
-git submodule init
-git submodule update
-
-#./nord-gnome-terminal/src/nord.sh
