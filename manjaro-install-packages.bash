dba6946132608e71dd05b02e05fdf042ff34f870
David Walker <dwalker0044@gmail.com>
2022-12-23 13:20:54 +0000

    manjaro - added ssh key generation.

--- manjaro-install-packages.bash
+++ manjaro-install-packages.bash
@@ -1,19 +1,20 @@
 sudo pacman-mirrors --fasttrack && sudo pacman -Syyu
 
-# Install nonfree graphics drivers.
+# install nonfree graphics drivers.
 sudo mhwd -a pci nonfree 0300
 
-sudo pacman -S clang
-sudo pacman -S ninja
-sudo pacman -S gdb
-sudo pacman -S make
-sudo pacman -S neovim
-sudo pacman -S python-pip
-sudo pacman -S python-poetry
-sudo pacman -S syncthing
-
-# Toolchain for arm cortex-m3 development:
-# sudo pacman -S qemu arm-none-eabi-gcc arm-none-eabi-gdb
+sudo pacman -S \
+clang \
+ninja \
+gdb \
+make \
+neovim \
+python-pip \
+python-poetry \
+syncthing
 
 systemctl --user enable syncthing.service
 systemctl --user start syncthing.service
+
+# generate ssh-key
+ssh-keygen -t ed25519
